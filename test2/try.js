68b734c60328240f923ec7d02ae866224a3626e7
limsohee <limsohee0521@gmail.com>
2022-05-10 17:41:28 +0900

    js 코드 뜯어보고 질문 정리 220510

--- test2/try.js
+++ test2/try.js
@@ -1,5 +1,6 @@
 consoleText(["Hello Uni", "This is Us","Do or Not","Daisy","Spring Snow"], "text", ["skyblue","red","black", "lightblue","rebeccapurple"]);
-//  consoleText라는 함수에 변수를 지정
+
+//   consoleText라는 함수에 변수를 지정
 // *? consoleText라는 함수를 맨위에다 놔도 작동이 되나?
 
 function consoleText(world, id, colors){
@@ -55,7 +56,8 @@ window.setInterval(function(){
       ? 배열에서 첫번째 요소를 제거하고 
       * and returns it.
       ? it을 반환한다 
-      todo 여기서 it 이 뭘까
+      todo 여기서 it 이 뭘까 (배열인듯)
+
       * If the array is empty, 
       ? 만약 그 배열이 비면
       * undefined is returned and the array is not modified.
@@ -83,6 +85,8 @@ window.setInterval(function(){
     target.setAttribute("style","color:" + colors[0]);
     //target이라는 요소에 setAttribute에 style이라는 속성 부여, "color:"라는 string값에 + 배열 colors[0]값을 부여 
     letterCount += x; //! += 이라는 연산자는 어떤 연산자일까?
+    // memo 더하기 연산자래 
+    //! += 와 =+ 가 다름
     // todo +=이라는 연산자 찾아보기
     waiting = false; // waiting에 false대입
   },1000) // 그걸 1000에 한번 반복해줘 // * 여기도 시간이 들어가는 걸 보니 setInterval 처럼 반복인가봄 
@@ -99,7 +103,7 @@ window.setInterval(function(){
       //?메서드가 정확하게 뭐야? 내장함수?
 
       x = -1;
-      // x에 -1 대입 //?음수도 대입이 되는 구나
+      // x에 -1 대입 // memo 음수도 대입이 되는 구나
       
       letterCount += x;
       // todo += 연산자 찾아보기
@@ -108,15 +112,41 @@ window.setInterval(function(){
 
     },1000)// 그걸 1000에 한번 반복해줘
   } else if(waiting === false ){// 그것도 아니고 그냥 waiting이 false이기만 하면
+    target.innerHTML = words[0].substring(0, letterCount)
+    /* 
+    *The zero-based index number indicating the beginning of the substring.
+    ^ 부분 문자열(substring)의 시작을 알리는 제로베이스 인덱스
 
+    *Returns the substring at the specified location within a String object.
+    ^ subdtring을 특정 위치에 반환 (문자열 객체 이내에)
+    */
+    letterCount += x;
+    // letterCount 에 += x를 해줘 // memo 더하기 할당이래
   }
 }, 120);
-// memo window에서 뭘 할건데 그걸 120에 한번 해줘
-//* window에도 setInterval이 있구나
+//* window에서 뭘 할건데 그걸 120에 한번 해줘
+//  memo window에도 setInterval이 있구나
 
 
-window.setInterval(function(){},400);
-//window에서 뭘할건데 그걸 400에 한번 해줘
+window.setInterval(function(){
+
+  if(visible === true){
+    // 만약에 visible값이 true이면
+    con.className = "console-underscore hidden";
+    // con의 객체 중 className 에 "console-underscore hidden"을 대입 해줘
+    //? console-underscore hidden은 무엇일까?
+    visible = false;
+    // visible의 값에 false를 대입해줘
+  }else{// 아니면
+    con.className = "console-underscore"
+    // con의 객체중 className dp "console-underName"을 대입
+    visible=true;
+    // visible에 true를 대입
+  }
+  // memo 이거 반복되겠다
+
+},400);
+// *window에서 뭘할건데 그걸 400에 한번 해줘
 
 
 }
